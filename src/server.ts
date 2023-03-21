fc26120069223fce235ae6516a63340e1288d3b2
Jean Michel Biz <jeanmbiz@hotmail.com>
2023-03-21 11:16:51 -0300

    added diagram and finished servidor configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import app from "./app"
 import AppDataSource from "./data-source"
 
-AppDataSource.initialize().then(async ()=>{
-    console.log('Database conected.')
+AppDataSource.initialize().then(async () => {
+    console.log("Database connected.")
 
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, ()=>{
-        console.log(`App is running on https://localhost:${PORT}`)
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+      console.log(`App is running on https://localhost:${PORT}`)
     })
-}).catch(err => console.log(err))
+  })
+  .catch((err) => console.error(err));
