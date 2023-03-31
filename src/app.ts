3d6a33908d0c10b4c9c89d286a5c9b51b4cd870b
Jean Michel Biz <jeanmbiz@hotmail.com>
2023-03-31 15:38:32 -0300

    implemented cors

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,12 @@ import handleError from "./errors/handleError";
 import customerRoutes from "./routes/customers.routes";
 import sessionRoutes from "./routes/session.routes";
 import userRoutes from "./routes/user.routes";
+import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/login", sessionRoutes);
